7340a64dae81530fad0f701e82b4e9199325a725
ppichier <pierantonio.pichierri@gmail.com>
2019-12-29 16:20:04 +0100

    [ADD] product CRUD

--- routes/user.js
+++ routes/user.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const { requireSignin } = require("../controllers/auth");
+const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
 
 const { userById } = require("../controllers/user");
 
-router.get("/secret/:userId", requireSignin, (req, res) => {
+router.get("/secret/:userId", requireSignin, isAuth, isAdmin, (req, res) => {
   res.json({
     user: req.profile
   });
 });
 
-router.param("userId", userById);
+router.param("userId", userById); //make the user.profile available in the request
 
 module.exports = router;
