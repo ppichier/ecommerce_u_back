959629b72d0d8e10fcba83cf272c7c56e69b9e2e
ppichier <pierantonio.pichierri@gmail.com>
2019-12-30 19:34:50 +0100

    [ADD] product / categories CRUD

--- routes/product.js
+++ routes/product.js
@@ -6,7 +6,11 @@ const {
   productById,
   read,
   remove,
-  update
+  update,
+  list,
+  listRelated,
+  listCategories,
+  listBySearch
 } = require("../controllers/product");
 const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
 const { userById } = require("../controllers/user");
@@ -28,6 +32,11 @@ router.put(
   isAdmin,
   update
 );
+
+router.get("/products", list);
+router.get("/products/related/:productId", listRelated);
+router.get("/products/categories", listCategories); //list of categories use for products
+router.post("/products/by/search", listBySearch);
 
 router.param("userId", userById);
 router.param("productId", productById);
